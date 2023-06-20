6ea8629a9110be1cce2d38a12e6f3635b2f2f696
StoyanBanov <sbanov05@gmail.com>
2023-06-20 18:30:08 +0300

    add title, fix player1 attack hit box

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 export function attack(attacker, target) {
     const attack = attacker.currentAttack
-    if (attacker.position.x - attack.offset.x + (attacker.orientation == 'right' ? attack.width : 0) <= target.position.x + target.width &&
-        attacker.position.x - attack.offset.x + (attacker.orientation == 'right' ? attack.width : 0) >= target.position.x &&
+    if (attacker.position.x - attack.offset.x <= target.position.x + target.width &&
+        attacker.position.x - attack.offset.x >= target.position.x &&
         (attacker.position.y - attack.offset.y + attack.height <= target.position.y + target.height &&
             attacker.position.y - attack.offset.y + attack.height >= target.position.y ||
             attacker.position.y - attack.offset.y <= target.position.y + target.height &&
